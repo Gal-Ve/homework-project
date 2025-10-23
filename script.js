a8e86d73e1a1cfd96607cabe3525e6f4e0c264c1
Gal-Ve <galvejhunel@gmail.com>
2025-10-24 00:47:13 +0800

    add carousel

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const sidenav = document.querySelector(".sidenav");
 const close = document.querySelector(".close")
 const galleryButton = document.querySelector("#view")
 const galleryView = document.querySelector(".gallery-frame");
+const galleryClose = document.querySelector(".gallery-close");
+const images = document.querySelectorAll(".carousel-image");
+const thumbnails = document.querySelectorAll(".thumbnail");
+const prevBtn = document.querySelector(".carousel-nav.prev");
+const nextBtn = document.querySelector(".carousel-nav.next");
 
 const displaySide = () => {
   sidenav.classList.remove("hide")
@@ -22,3 +27,61 @@ view.addEventListener('click', ()=>{
   galleryView.classList.remove("hide");
   galleryView.classList.add("show");
 })
+
+galleryClose.addEventListener('click', ()=>{
+  galleryView.classList.remove("show");
+  galleryView.classList.add("hide");
+})
+
+let currentIndex = 0;
+
+const loadedImages = new Set([0]);
+
+function showImage(index) {
+ if (index >= images.length) {
+  currentIndex = 0;
+ } else if (index < 0) {
+  currentIndex = images.length - 1;
+ } else {
+  currentIndex = index;
+ }
+
+ if (loadedImages.has(currentIndex)) {
+  images.forEach((img, i) => {
+   img.classList.toggle("active", i === currentIndex);
+  });
+ } else {
+  const img = images[currentIndex];
+  if (img.complete) {
+   loadedImages.add(currentIndex);
+   images.forEach((image, i) => {
+    image.classList.toggle("active", i === currentIndex);
+   });
+  }
+ }
+
+ thumbnails.forEach((thumb, i) => {
+  thumb.classList.toggle("active", i === currentIndex);
+ });
+}
+
+images.forEach((img, index) => {
+ img.addEventListener("load", () => {
+  loadedImages.add(index);
+ });
+ img.addEventListener("error", () => {
+  console.error(`Failed to load image ${index}`);
+  loadedImages.add(index);
+ });
+});
+
+
+prevBtn.addEventListener("click", () => showImage(currentIndex - 1));
+nextBtn.addEventListener("click", () => showImage(currentIndex + 1));
+
+thumbnails.forEach((thumbnail) => {
+ thumbnail.addEventListener("click", () => {
+  const index = parseInt(thumbnail.dataset.index);
+  showImage(index);
+ });
+});
